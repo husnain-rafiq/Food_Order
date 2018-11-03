e3ccbcb276eac3dba8e243e22c8cf7d7a0239440
husnain-rafiq <husnainrafiq12@gmail.com>
2018-11-03 15:17:51 +0500

    Initial commit

---
+++ app/src/main/java/com/example/dell/foodorder/Common/Common.java
@@ -0,0 +1,6 @@
+package com.example.dell.foodorder.Common;
+
+public class Common {
+
+    //public static User currentUser;
+}
---
+++ app/src/main/java/com/example/dell/foodorder/Interface/ItemClickListener.java
@@ -0,0 +1,7 @@
+package com.example.dell.foodorder.Interface;
+
+import android.view.View;
+
+public interface ItemClickListener {
+    void onClick(View view,int position,Boolean isLongClick);
+}
